56b41536e7e9aacfaf283d1e23b933bfe3fe2ec1
Dominic Jones-Jackson <44783114+sethveeper@users.noreply.github.com>
2019-09-03 09:03:30 -0500

    Smol Updates

--- Chpt3_Assignment.sql
+++ Chpt3_Assignment.sql
@@ -1,6 +1,7 @@
 USE dj0803805;
 GO
 
+-- Question 1
 SELECT ProductCode AS Code, 
 		ProductName AS [Product Name], 
 		ListPrice AS [Listed Price], 
@@ -8,16 +9,19 @@ SELECT ProductCode AS Code,
 		FROM PRODUCTS
 		ORDER BY ListPrice DESC;
 
+-- Question 2
 SELECT LastName + ', ' + FirstName AS Name
 		FROM Customers;
-		
+
+-- Question 3
 SELECT ProductName AS [Product Name], 
 		ListPrice AS [Listed Price], 
 		DateAdded AS Date
 		FROM PRODUCTS
 		WHERE ListPrice > 500 AND ListPrice < 2000
 		ORDER BY ListPrice DESC;
-		
+
+-- Question 4
 SELECT ProductName AS [Product Name], 
 		ListPrice AS [Listed Price], 
 		DiscountPercent AS [Discount Percent],
@@ -25,6 +29,7 @@ SELECT ProductName AS [Product Name],
 		ListPrice - (ListPrice * (DiscountPercent / 100)) AS [Discount Price]
 		FROM PRODUCTS;
 
-SELECT OrderID, OrderDate, ShipDate 
+-- Question 5
+SELECT OrderID AS [Order ID], OrderDate AS [Order Date], ShipDate AS [Ship Date] 
 	FROM Orders
 	WHERE ShipDate IS NULL;
--- SQLQuery_InnerJoin.sql
+++ SQLQuery_InnerJoin.sql
@@ -3,7 +3,7 @@ GO
 
 -- This is a demonstration of how Inner Joins work.
 
-Select InvoiceNumber, VendorName
+SELECT InvoiceNumber, VendorName
 	FROM Vendors JOIN Invoices
 		ON Vendors.VendorID = Invoices.VendorID;
 		-- Essentially, it's looking for anything that
@@ -16,3 +16,13 @@ SELECT InvoiceNumber, InvoiceDate,
 		ON (Invoices.InvoiceID = LineItems.InvoiceID)
 		AND (Invoices.InvoiceTotal > LineItems.InvoiceLineItemAmount)
 	ORDER BY InvoiceNumber;
+
+-- This one is apparently called a "Self Join".
+-- It returns common cities between vendors.
+SELECT DISTINCT Vendors1.VendorName, Vendors1.VendorCity,
+	Vendors1.VendorState
+	FROM Vendors AS Vendors1 JOIN Vendors AS Vendors2
+		ON (Vendors1.VendorCity = Vendors2.VendorCity) 
+		AND (Vendors1.VendorState = Vendors2.VendorState) 
+		AND (Vendors1.VendorID <> Vendors2.VendorID)
+	ORDER BY Vendors1.VendorState, Vendors1.VendorCity;
