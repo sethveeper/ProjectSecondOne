982f3ad9bab920fd9d06e4ac75e5a8a3dad68151
Dominic Jones-Jackson <44783114+sethveeper@users.noreply.github.com>
2019-09-03 08:48:09 -0500

    Inner Joins
    An "Inner Join" lets us compare data from different tables via a common key.

--- SQLQuery_InnerJoin.sql
+++ SQLQuery_InnerJoin.sql
@@ -8,3 +8,11 @@ Select InvoiceNumber, VendorName
 		ON Vendors.VendorID = Invoices.VendorID;
 		-- Essentially, it's looking for anything that
 		-- shares a VendorID on both tables.
+
+-- This one only returns certain lines.
+SELECT InvoiceNumber, InvoiceDate,
+	InvoiceTotal, InvoiceLineItemAmount
+	FROM Invoices JOIN InvoiceLineItems AS LineItems
+		ON (Invoices.InvoiceID = LineItems.InvoiceID)
+		AND (Invoices.InvoiceTotal > LineItems.InvoiceLineItemAmount)
+	ORDER BY InvoiceNumber;
