abf06c072976caa38451a56b22dd88026ed68b8e
Dominic Jones-Jackson <44783114+sethveeper@users.noreply.github.com>
2019-10-08 09:08:26 -0500

    Update Week07.sql
    Done! That wasn't so bad.

--- Week07.sql
+++ Week07.sql
@@ -6,6 +6,11 @@ Write the statement needed to add Cases to the categories in the database.
 Code the INSERT statement so SQL Server automatically generates the value for the CategoryID column.
 */
 
+INSERT INTO Categories (CategoryName)
+VALUES ('Cases');
+
+SELECT * FROM Categories
+ORDER BY CategoryID DESC;
 
 /*2.	During a meeting, it was decided that instead of Cases, 
 the Guitar Shop would start using the term Guitar Cases for these items. 
@@ -13,7 +18,12 @@ Write the statement that will update the name of the category Cases to Guitar Ca
 To be sure you are updating the right row, use the CategoryID in your statement.
 */
 
+UPDATE Categories
+	SET CategoryName = 'Guitar Cases'
+WHERE CategoryID = 5;
 
+SELECT * FROM Categories
+ORDER BY CategoryID DESC;
 
 /*3.	Some new items have come into the Guitar Shop and need to be added to the database. 
 Write the statement that will add this product:
@@ -27,10 +37,26 @@ DiscountPercent:	0
 DateAdded:	Today’s date/time.
 */
 
+INSERT INTO Products
+	(CategoryID, ProductCode, ProductName, Description, ListPrice, DiscountPercent, DateAdded)
+VALUES (4, 'dgx_680', 'Yamaha DGX 680 102-Key Digital Piano', 'Long description to come.', 799.99, 0, GETDATE());
+
+SELECT TOP 5 * FROM Products
+	ORDER BY ProductID DESC;
 
 /*4.	It was later determined that the new product dgx_680 would have a discount percentage of 30%. 
 Write the statement that will update the product in the database. 
 */
+
+UPDATE Products
+	SET DiscountPercent = 30
+WHERE ProductCode = 'dgx_680';
+/* Why use the product code instead of the ID? I didn't want to assume what the ID would be,
+as that would be untenable in a bigger database with perhaps hundreds or thousands of products.
+But I've been given the product code, and I can safely assume that it's still sufficiently unique. */
+
+SELECT TOP 5 * FROM Products
+	ORDER BY ProductID DESC;
 
 /*5.	A new customer needs to be added to the database so that their orders can be tracked. 
 Add the following customer to the Guitar Shop’s list:
@@ -40,3 +66,10 @@ Password:	(empty string)
 FirstName:	John
 LastName:	Snyder
 */
+
+INSERT INTO Customers
+	(EmailAddress, Password, FirstName, LastName)
+VALUES ('john.snyder@microsoft.com', '', 'John', 'Snyder');
+
+SELECT TOP 5 * FROM Customers
+ORDER BY CustomerID DESC;
