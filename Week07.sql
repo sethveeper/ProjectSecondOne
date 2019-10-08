eb722713d34b46149eacee296213e34e52426f1e
Dominic Jones-Jackson <44783114+sethveeper@users.noreply.github.com>
2019-10-08 08:01:59 -0500

    Create Week07.sql
    Hopefully we can have this finished by tonight.

---
+++ Week07.sql
@@ -0,0 +1,38 @@
+/*1.	The Guitar Shop is going to start carrying cases as part of their inventory. 
+Write the statement needed to add Cases to the categories in the database. 
+Code the INSERT statement so SQL Server automatically generates the value for the CategoryID column.
+*/
+
+/*2.	During a meeting, it was decided that instead of Cases, 
+the Guitar Shop would start using the term Guitar Cases for these items. 
+Write the statement that will update the name of the category Cases to Guitar Cases. 
+To be sure you are updating the right row, use the CategoryID in your statement.
+*/
+
+
+
+/*3.	Some new items have come into the Guitar Shop and need to be added to the database. 
+Write the statement that will add this product:
+ProductID:	The next automatically generated ID 
+CategoryID:	4
+ProductCode:	dgx_680
+ProductName:	Yamaha DGX 680 102-Key Digital Piano
+Description:	Long description to come.
+ListPrice:	799.99
+DiscountPercent:	0
+DateAdded:	Today’s date/time.
+*/
+
+
+/*4.	It was later determined that the new product dgx_680 would have a discount percentage of 30%. 
+Write the statement that will update the product in the database. 
+*/
+
+/*5.	A new customer needs to be added to the database so that their orders can be tracked. 
+Add the following customer to the Guitar Shop’s list:
+
+EmailAddress:	john.snyder@microsoft.com
+Password:	(empty string)
+FirstName:	John
+LastName:	Snyder
+*/
